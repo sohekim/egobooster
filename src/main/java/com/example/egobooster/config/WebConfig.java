59ce544ac384c426edbae33ffe37acaf27e213fe
Sohee Kim <kim66s@mtholyoke.edu>
2021-03-26 22:01:38 +0900

    fix swagger bug

--- src/main/java/com/example/egobooster/config/WebConfig.java
+++ src/main/java/com/example/egobooster/config/WebConfig.java
@@ -7,21 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//  @Override
-//  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//    registry.addResourceHandler("/webjars/**")
-//        .addResourceLocations("/webjars/**")
-//        .resourceChain(false);
-//    registry.setOrder(1);
-//  }
-
   @Override
   public void addResourceHandlers(final ResourceHandlerRegistry registry) {
     registry.addResourceHandler("swagger-ui.html")
         .addResourceLocations("classpath:/META-INF/resources/");
-
     registry.addResourceHandler("/webjars/**")
         .addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
-
 }
